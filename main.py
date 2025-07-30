af31c87e12340763b608fb1dc01bee0f699531e8
Aligoku1200 <106703834+Aligoku1200@users.noreply.github.com>
2025-07-30 18:21:59 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,22 +7,23 @@ os.system("clear")
 Ç.login("xzv@usbc.be","GOKU12")
 TT="8227089815:AAEAWDTBkzNtTfuf0PfFZvh-wEFAKSihU2w"
 TC="8100160592"
+U=[]
 L=Ç.get_from_code("http://aminoapps.com/p/prd2qm")
 C=L.comId
 O=L.objectId
 S=aminoli.SubClient(comId=C,profile=Ç.profile)
-X1=S.get_chat_messages(chatId=O,size=1).messageId
-while True:
-	for X2 in X1:
-		X3=S.get_message_info(chatId=O,messageId=X2)
-		X4=X3.messageType
-		if X4==101:
-			XX=" واحد دخل الشات الحقه"
-			url = f"https://api.telegram.org/bot{TT}/sendMessage"
-			params = {"chat_id": TC,"text": XX,"parse_mode": "HTML"}
-			response = requests.post(url, params=params)
-			print(response.json())
-			pass
+X1=S.get_chat_users(chatId=O,start=0,size=100).userId
+U.apend(X1)
+while 1:
+	X2=S.get_chat_users(chatId=O,start=0,size=100)
+	if X2==U:
+		print("نوبة فاضية")
+		pass
+	elif X2!=U:
+		XX=" واحد دخل الشات الحقه"
+		url = f"https://api.telegram.org/bot{TT}/sendMessage"
+		params = {"chat_id": TC,"text": XX,"parse_mode": "HTML"}
+		response = requests.post(url, params=params)
+		print(0)
+		pass
 	time.sleep(60)
-			
-	
