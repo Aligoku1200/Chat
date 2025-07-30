28bcef87b5d54b15259cef771d8135eb0a276431
Aligoku1200 <106703834+Aligoku1200@users.noreply.github.com>
2025-07-30 18:06:46 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ os.system("pip install amino.li")
 os.system("pip install requests")
 import aminoli,time,requests 
 os.system("clear")
-Ç=aminoli.Client(devicId="19EC08F654EDA29F919257B18C9B7F4985D59C48BE179EE53DA63532F7588BB92505525E82A3E707C0")
+Ç=aminoli.Client()
 Ç.login("xzv@usbc.be","GOKU12")
 TT="8227089815:AAEAWDTBkzNtTfuf0PfFZvh-wEFAKSihU2w"
 TC="8100160592"
